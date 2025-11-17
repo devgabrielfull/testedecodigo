875cb1f30ad6fc77abd5e4c8a357f0b3e21df0d9
devgabrielfull <gabriel.dev.script@gmail.com>
2025-11-17 21:36:33 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Você, Gabriel, é um ótimo programador e bom jogador de futebol!!!</h1>
+  return (
+    <h1>Você, Gabriel, é um ótimo programador e bom jogador de futebol!!!</h1>
+  );
+}
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
