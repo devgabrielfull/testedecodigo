af36267b1f91d402cfb03a7806c95dade4ead85e
devgabrielfull <gabriel.dev.script@gmail.com>
2026-05-24 21:42:30 +0000

    alt

--- infra/database.js
+++ infra/database.js
@@ -4,8 +4,9 @@ function query() {
     const client = new Client();
     await client.connect();
     const result = await client.query(queryObject);
+    
 }
 
 export default {
     query: query,
-};
+}; 
